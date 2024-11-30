5b3ad60c02759e415431fd0a31e6073299d5ed71
Filipe Piazza <filipepiazza@gmail.com>
2024-11-30 09:49:20 -0300

    part 7 router/hooks exercises

--- src/App.jsx
+++ src/App.jsx
@@ -739,38 +739,249 @@
 
 //-------------------------------------------------------------------------
 
-import AnecdoteNotification from './components/AnecdoteNotification'
-import Anecdotes from './components/Anecdotes'
-import Filter from './components/Filter'
-import NewEntry from './components/NewEntry'
-import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { initializeAnecdotes } from './reducers/anecdoteReducer'
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
-import { useReducer, useContext } from 'react'
-import counterReducer from './reducers/counterReducer'
-import CounterContext from './CounterContext'
-import { useCounterValue, useCounterDispatch } from './CounterContext'
+// import AnecdoteNotification from './components/AnecdoteNotification'
+// import Anecdotes from './components/Anecdotes'
+// import Filter from './components/Filter'
+// import NewEntry from './components/NewEntry'
 
-const App = () => {
-  //redux structure
-  // const dispatch = useDispatch()
-  // useEffect(() => {
-  //   dispatch(initializeAnecdotes())
-  // }, [])
+// const App = () => {
+//   //redux structure
+//   // const dispatch = useDispatch()
+//   // useEffect(() => {
+//   //   dispatch(initializeAnecdotes())
+//   // }, [])
+
+//   return (
+//     <>
+//       <Filter />
+//       <AnecdoteNotification />
+//       <Anecdotes />
+//       <NewEntry />
+//     </>
+//   )
+// }
+
+// export default App
+
+import { useState } from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useParams,
+  useNavigate,
+  useMatch,
+} from 'react-router-dom'
+import { useField } from './hooks'
+
+const Menu = () => {
+  const padding = {
+    paddingRight: 5,
+  }
+  return (
+    <div>
+      <Link style={padding} to="/">
+        anecdotes
+      </Link>
+      <Link style={padding} to="/create">
+        create
+      </Link>
+      <Link style={padding} to="/about">
+        about
+      </Link>
+    </div>
+  )
+}
+
+const Anecdote = ({ anecdote }) => {
+  return (
+    <div>
+      <h2>{anecdote.content}</h2>
+      <div>{anecdote.author}</div>
+      <div>{anecdote.info}</div>
+      <div>{anecdote.votes}</div>
+    </div>
+  )
+}
+
+const AnecdoteList = ({ anecdotes }) => (
+  <div>
+    <h2>Anecdotes</h2>
+    <ul>
+      {anecdotes.map((anecdote) => (
+        <li key={anecdote.id}>
+          <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+        </li>
+      ))}
+    </ul>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>About anecdote app</h2>
+    <p>According to Wikipedia:</p>
+
+    <em>
+      An anecdote is a brief, revealing account of an individual person or an
+      incident. Occasionally humorous, anecdotes differ from jokes because their
+      primary purpose is not simply to provoke laughter but to reveal a truth
+      more general than the brief tale itself, such as to characterize a person
+      by delineating a specific quirk or trait, to communicate an abstract idea
+      about a person, place, or thing through the concrete details of a short
+      narrative. An anecdote is "a story with a point."
+    </em>
+
+    <p>
+      Software engineering is full of excellent anecdotes, at this app you can
+      find the best and add more.
+    </p>
+  </div>
+)
+
+const Footer = () => (
+  <div>
+    Anecdote app for <a href="https://fullstackopen.com/">Full Stack Open</a>.
+    See{' '}
+    <a href="https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js">
+      https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js
+    </a>{' '}
+    for the source code.
+  </div>
+)
+
+const CreateNew = (props) => {
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
+
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    props.addNew({
+      content: content.value,
+      author: author.value,
+      info: info.value,
+      votes: 0,
+    })
+    navigate('/')
+    props.notify(`new anecdote ${content.value} was created`)
+  }
+
+  const reset = () => {
+    content.onReset()
+    author.onReset()
+    info.onReset()
+  }
 
   return (
-    <>
-      <Filter />
-      <AnecdoteNotification />
-      <Anecdotes />
-      <NewEntry />
-    </>
+    <div>
+      <h2>create a new anecdote</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          content
+          <input {...content} />
+        </div>
+        <div>
+          author
+          <input {...author} />
+        </div>
+        <div>
+          url for more info
+          <input {...info} />
+        </div>
+        <button>create</button>
+      </form>
+      <button onClick={reset}>reset</button>
+    </div>
+  )
+}
+
+const AnecdoteNotification = ({ notification }) => {
+  if (!notification) return null
+
+  const style = {
+    border: 'solid',
+    padding: 10,
+    borderWidth: 1,
+  }
+
+  return <div style={style}>{notification}</div>
+}
+
+const App = () => {
+  const [anecdotes, setAnecdotes] = useState([
+    {
+      content: 'If it hurts, do it more often',
+      author: 'Jez Humble',
+      info: 'https://martinfowler.com/bliki/FrequencyReducesDifficulty.html',
+      votes: 0,
+      id: 1,
+    },
+    {
+      content: 'Premature optimization is the root of all evil',
+      author: 'Donald Knuth',
+      info: 'http://wiki.c2.com/?PrematureOptimization',
+      votes: 0,
+      id: 2,
+    },
+  ])
+
+  const [notification, setNotification] = useState('')
+
+  const match = useMatch('/anecdotes/:id')
+  const anecdote = match
+    ? anecdotes.find((anecdote) => anecdote.id === Number(match.params.id))
+    : null
+
+  const addNew = (anecdote) => {
+    anecdote.id = Math.round(Math.random() * 10000)
+    console.log('anecdote', anecdote)
+
+    setAnecdotes(anecdotes.concat(anecdote))
+  }
+
+  const notify = (message) => {
+    setNotification(message)
+    setTimeout(() => {
+      setNotification(null)
+    }, 5000)
+  }
+
+  const anecdoteById = (id) => anecdotes.find((a) => a.id === id)
+
+  const vote = (id) => {
+    const anecdote = anecdoteById(id)
+
+    const voted = {
+      ...anecdote,
+      votes: anecdote.votes + 1,
+    }
+
+    setAnecdotes(anecdotes.map((a) => (a.id === id ? voted : a)))
+  }
+
+  return (
+    <div>
+      <Menu />
+      <AnecdoteNotification notification={notification} />
+      <Routes>
+        <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
+        <Route
+          path="/anecdotes/:id"
+          element={<Anecdote anecdote={anecdote} />}
+        />
+        <Route
+          path="/create"
+          element={<CreateNew addNew={addNew} notify={notify} />}
+        />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <h1>Software anecdotes</h1>
+      <Footer />
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -57,6 +57,7 @@ import App from './App'
 import store from './store'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { NotificationContextProvider } from './NotificationContext'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 const queryClient = new QueryClient()
 
@@ -64,7 +65,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <QueryClientProvider client={queryClient}>
       <NotificationContextProvider>
-        <App />
+        <Router>
+          <App />
+        </Router>
       </NotificationContextProvider>
     </QueryClientProvider>
   </Provider>
